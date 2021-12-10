119c87bc8ec01143ac86ea9399eae85fccf19211
Vladimir-Sluchko <vovan1991.s@gmail.com>
2021-12-11 01:42:18 +0300

    Reshenie zadachi 3

---
+++ home_work_1/src/Number3.java
@@ -0,0 +1,13 @@
+public class Number3 {
+    public static void main(String[] args){
+        System.out.println(Math.pow(2,4));// возводи число 2 в степень 4, ответ 16
+        System.out.println(Math.max(14,30));// выводит максимальное значени
+        System.out.println(Math.sqrt(144)); //извлекает квадратный корень
+        System.out.println(Math.random()); // рандомное число от 0 включая 0 до 1 но не включая 1 (0<=a<1)
+        System.out.println(Math.round(1.3));// округляет по правилам математики до целого числа
+        System.out.println(Math.floor(1.8));// округляет в наименьшую сторону до целого числа
+        System.out.println(Math.ceil(1.2));// округляет в большую сторону до целого числа
+        
+
+    };
+}
