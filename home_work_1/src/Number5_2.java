0a881acaac0a9e3a70d679032fc427ecf0fd4c8a
Vladimir-Sluchko <vovan1991.s@gmail.com>
2021-12-11 03:27:35 +0300

    zadacha 5_2

---
+++ home_work_1/src/Number5_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Number5_2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name = in.next();
+        String vasia = "Вася";
+        String anasts = "Анастасия";
+        if (name.equals(vasia)){
+            System.out.println("Привет!");
+            System.out.println("Я тебя так долго ждал");
+        }
+        else if (name.equals(anasts)){
+             System.out.println("Я тебя так долго ждал");
+             }
+        else {
+            System.out.println("Добрый день а вы кто");
+        }
+
+    }
+}
