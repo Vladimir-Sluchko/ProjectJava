b768e1b520eba0220d0f101447c75b280a1dce84
Vladimir-Sluchko <vovan1991.s@gmail.com>
2021-12-22 18:15:27 +0300

    Dobavlena zadacha s rekursiei

--- home_work_2/src/README.txt
+++ home_work_2/src/README.txt
@@ -1,13 +1,14 @@
-class Number1_1    решает задание 1.1.
-class Number1_2    решает задание 1.2.
-class Number1_3    решает задание 1.3.
-class Number1_4_1  решает задание 1.4.1.
-class Number1_4_2  решает задание 1.4.2.
-class Number1_4_3  решает задание 1.4.3.
-class Number1_4_4  решает задание 1.4.4.
-class Number1_5    решает задание 1.5.
-class ArraysUtils  решает задание 2.1
-class Number2_2    решает задание 2.2.
-class Number2_4    решает задание 2.4.
+class Number1_1          решает задание 1.1.
+class Number1_1rekurs    решает задание 1.1.(рекурсия)
+class Number1_2          решает задание 1.2.
+class Number1_3    	 решает задание 1.3.
+class Number1_4_1  	 решает задание 1.4.1.
+class Number1_4_2  	 решает задание 1.4.2.
+class Number1_4_3  	 решает задание 1.4.3.
+class Number1_4_4  	 решает задание 1.4.4.
+class Number1_5    	 решает задание 1.5.
+class ArraysUtils  	 решает задание 2.1
+class Number2_2    	 решает задание 2.2.
+class Number2_4    	 решает задание 2.4.
 
 
--- home_work_2/src/loops/Number1_1rekurs.java
+++ home_work_2/src/loops/Number1_1rekurs.java
@@ -1,4 +1,39 @@
+//1. Циклы. Для преобразования строки в число можно использовать http://proglang.su/java/numbers-parseint,
+// если хотите можете добавить проверки на корректность ввода данных. Число положительное, максимум long:
+//    1.1. Перемножить числа от 1 до числа (включительно) введенного через аргумент к исполняемой программе.
+//    Есть нюанс с переполнением, можно добавить проверки и сообщения пользователю. Пример: Ввели 5,
+//    должно получиться в консоли: 1 * 2 * 3 * 4 * 5 = ответ
+//    	1.1.2.* Используя рекурсию
 package loops;
 
 public class Number1_1rekurs {
+    public static void main(String[] args) {
+        long n = 1;
+        System.out.println("Число которое записано в аргументе " + args[0]);
+        try {
+            long chislo =Long.parseLong(args[0]);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("Пользователь ввёл некорректные данные.");
+            return;
+        }
+        long chislo =Long.parseLong(args[0].strip());
+        if (chislo > 20) {
+            System.out.println("Значение итоговой переменной переполнено, попробуйте заново и введите число меньше!");
+            return;
+        }
+
+        System.out.println(rekurs(chislo));
+    }
+
+    public static long rekurs (long n){
+        if (n == 1){
+            System.out.print(n + " = ");
+            return 1;
+        }
+            System.out.print(n + " * ");
+        return n * rekurs(n-1);
+
+    }
+
 }
